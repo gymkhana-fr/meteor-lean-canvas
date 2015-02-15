f0cdd897daa7809762a69c1e24ba8b1c4878c47b
pointbar <stephane.langlois@scopyleft.fr>
2015-02-15 17:22:23 +0100

    set the package

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lean canvas template
+
--- package.js
+++ package.js
@@ -1,22 +1,25 @@
 Package.describe({
   name: 'pntbr:lean-canvas',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'Lean canvas template',
+  git: 'https://github.com/gymkhana-fr/meteor-lean-canvas',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
-  api.addFiles('pntbr:lean-canvas.js');
+  api.use(['templating', 'handlebars', 'bootstrap'], 'client');
+  api.addFiles([
+    'lib/client/canvas.css',
+    'lib/client/canvas.html',
+    'lib/client/content.css',
+    'lib/client/content.html',
+    'lib/client/content.js',
+  ], 'client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('pntbr:lean-canvas');
+  api.use('pntbr:lean-canvas', 'client');
   api.addFiles('pntbr:lean-canvas-tests.js');
 });
